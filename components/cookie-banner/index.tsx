1946393e3490ec33aecb4cb994eb4e3738db3544
unknown <bashabdulbash@gmail.com>
2025-09-24 07:16:50 +0100

    update

--- components/cookie-banner/index.tsx
+++ components/cookie-banner/index.tsx
@@ -32,7 +32,7 @@ const CookieBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-[10%] left-1/2 -translate-x-1/2 max-h-[130px] max-w-[1136px]  flex gap-6 items-center border border-[#17403A] rounded-[24px] p-6 w-full shadow-md bg-[#D4E8DB] z-50">
+    <div className="fixed bottom-4 inset-x-4 md:bottom-[10%] md:left-1/2 md:inset-x-auto md:-translate-x-1/2 md:w-full md:max-w-[1136px] md:max-h-[130px] flex flex-col md:flex-row gap-4 md:gap-6 items-start md:items-center  border border-[#17403A] rounded-[16px] md:rounded-[24px] p-4 md:p-6 w-full shadow-md bg-[#D4E8DB] z-50">
       <div className="flex-1">
         <h4 className="text-xl text-[#06212C] font-semibold">
           We’ve got cookies!
